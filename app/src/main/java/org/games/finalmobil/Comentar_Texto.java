fb15c102d03abd831c7614a5c8a73a3120d17693
Douglas-BM <douglas71328524@continental.edu.pe>
2022-06-24 18:18:04 -0500

    pantallas

---
+++ app/src/main/java/org/games/finalmobil/Comentar_Texto.java
@@ -0,0 +1,14 @@
+package org.games.finalmobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Comentar_Texto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comentar_texto);
+    }
+}
---
+++ app/src/main/java/org/games/finalmobil/Texto_Detalle.java
@@ -0,0 +1,14 @@
+package org.games.finalmobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Texto_Detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texto_detalle);
+    }
+}
